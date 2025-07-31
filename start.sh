5d96507b505426f1109d7cbb00473bc03dc7e613
sunsorady12 <sundante100@gmail.com>
2025-07-31 08:44:37 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-pip install -r requirements.txt
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies in isolated environment
+pip install --no-cache-dir -r requirements.txt
+
+# Run the bot
 python bot.py
